4d5e476aeea5c8ee3dcdd19fa6c1c7e0868c4e57
GregorBoxer-sudo <62080488+GregorBoxer-sudo@users.noreply.github.com>
2021-01-28 00:53:49 +0100

    Added Index and GUID-Gen

---
+++ dashboard.php
@@ -0,0 +1,45 @@
+<?php
+    session_abort();
+    session_start();
+    $uid = create_guid();
+    $uidHash = sha1($uid);
+    $_SESSION['uid'] = $uid;
+    
+    function create_guid() { // Create GUID (Globally Unique Identifier)
+      $guid = '';
+      $namespace = rand(11111, 99999);
+      $uid = uniqid('', true);
+      $data = $namespace;
+      $data .= $_SERVER['REQUEST_TIME'];
+      $data .= $_SERVER['HTTP_USER_AGENT'];
+      $data .= $_SERVER['REMOTE_ADDR'];
+      $data .= $_SERVER['REMOTE_PORT'];
+      $hash = strtoupper(hash('ripemd128', $uid . $guid . md5($data)));
+      $guid = substr($hash,  0,  4) . '-' .
+              substr($hash,  8,  2) .
+              substr($hash, 12,  2) . '-' .
+              substr($hash, 16,  4) . '-' .
+              substr($hash, 20, 4);
+      /*$guid = substr($hash,  0,  8) . '-' .
+              substr($hash,  8,  4) . '-' .
+              substr($hash, 12,  4) . '-' .
+              substr($hash, 16,  4) . '-' .
+              substr($hash, 20, 12);
+              */
+      return $guid;
+  }
+?>
+<html>
+    <head>
+      <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+      <h1> 
+        Willkommen beim private Messenger
+      </h1>
+      <br>
+      <br>
+        Deine NutzerID ist: <?php echo $_SESSION['uid'];?> 
+      <br>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>WiP</title>
+    <link rel="icon" href="https://lh3.googleusercontent.com/proxy/LLwGSJOeIeB9Xy10EjCXU5hhO4kjSL17llih51Mr1ydWaVFanvBBf5Ny_6jj9R0093i61P7OGuhsM6Ag9mWiyvPn9GqBaoIUAQRGDQkV18G5zb5y1OUuJ8VzjNl3VboWHJgBceKOQ1GqwGM5rJR42rLT8UKtu57A6M2V9g-aDQ">
+</head>
+
+<body>
+    <img src="https://img-comment-fun.9cache.com/media/a5Zm5my/aL9K67qR_700w_0.jpg">
+    <h1>Deae Customer, this service is currently work in progress and therefore not ready to use yet. <br> Stay excited!</h1>
+    <button onclick="window.location.href = 'dashboard.php'">trotzdem probieren!</button>
+</body>
+
+</html>
