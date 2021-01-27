e40d42b3b43ab039379451b152456ae13dea4e7b
GregorBoxer-sudo <62080488+GregorBoxer-sudo@users.noreply.github.com>
2021-01-28 00:56:35 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -20,12 +20,6 @@
               substr($hash, 12,  2) . '-' .
               substr($hash, 16,  4) . '-' .
               substr($hash, 20, 4);
-      /*$guid = substr($hash,  0,  8) . '-' .
-              substr($hash,  8,  4) . '-' .
-              substr($hash, 12,  4) . '-' .
-              substr($hash, 16,  4) . '-' .
-              substr($hash, 20, 12);
-              */
       return $guid;
   }
 ?>
