89ca347bfe32a7ca653869fc2e7c2e683ec8c44e
GregorBoxer-sudo <62080488+GregorBoxer-sudo@users.noreply.github.com>
2021-01-28 15:30:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <html>
-
 <head>
     <meta charset="utf-8">
     <title>WiP</title>
     <link rel="icon" href="https://lh3.googleusercontent.com/proxy/LLwGSJOeIeB9Xy10EjCXU5hhO4kjSL17llih51Mr1ydWaVFanvBBf5Ny_6jj9R0093i61P7OGuhsM6Ag9mWiyvPn9GqBaoIUAQRGDQkV18G5zb5y1OUuJ8VzjNl3VboWHJgBceKOQ1GqwGM5rJR42rLT8UKtu57A6M2V9g-aDQ">
 </head>
-
 <body>
     <img src="https://img-comment-fun.9cache.com/media/a5Zm5my/aL9K67qR_700w_0.jpg">
     <h1>Deae Customer, this service is currently work in progress and therefore not ready to use yet. <br> Stay excited!</h1>
@@ -15,5 +13,4 @@
     <br>
     <a href="https://docs.google.com/document/d/1AW7I1kLx_LlGN_nbQE43joOdSn5mnM2fA2ZUST9VvsM/edit?usp=sharing">schau doch bei Google-Docs vorbei!</a>
 </body>
-
 </html>
