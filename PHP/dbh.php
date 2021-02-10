adbde53e0e49d6257f21597e0ec5417a89866d6d
Tom <65157178+t0mbrn@users.noreply.github.com>
2021-02-10 23:20:57 +0100

    fix test

---
+++ PHP/dbh.php
@@ -0,0 +1,12 @@
+<?php
+    require 'session.php';
+    //Enter here your own attributes
+    $servername = "localhost";
+    $dBUsername = "tom";
+    $dBPassword = "123456";
+    $dBName = "db_m";
+    $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+    if (!$conn) {
+        die("Connection failed: ".mysqli_connect_error());
+        echo "Connection failed: ".mysqli_connect_error();
+    }
