ac50131ce3abde22a2fdbf31e33e299931b8f309
anthony lanzenberger <anthony.lanzenberger@gmail.com>
2021-05-25 03:26:19 +0200

    keylistener shift enter
    press shift enter to insert /n into input

--- JS/keyListener.js
+++ JS/keyListener.js
@@ -1,4 +1,6 @@
 document.addEventListener('keydown', (e) => {
-    if (e.code === "Enter")
+    if (e.code === "Enter" && e.shiftKey)
+        document.getElementById("messageInput").value += "/n";
+    if (e.code === "Enter" && !e.shiftKey)
         sendMessage();
 })
