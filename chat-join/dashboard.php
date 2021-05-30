d9fa2fd041a90993261e68f3fc22c68c9de55bdc
anthony lanzenberger <anthony.lanzenberger@gmail.com>
2021-05-30 17:21:59 +0200

    Update dashboard.php

--- chat-join/dashboard.php
+++ chat-join/dashboard.php
@@ -24,14 +24,14 @@
             function encryptKey() {
                 console.log("sakldjadslkdj: "+sessionStorage.length);
                 if (sessionStorage.length < 1) {
-                    let encrytionKey = "";
+                    let encryptionKey = "";
                     let chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
                     for (i = 0; i < 12; i++) {
                         randomElement = chars[Math.floor(Math.random()*chars.length)];
-                        encrytionKey += randomElement;
+                        encryptionKey += randomElement;
                     }
-                    sessionStorage.setItem("key", encrytionKey);
-                    return encrytionKey;
+                    sessionStorage.setItem("key", encryptionKey);
+                    return encryptionKey;
                 } else {
                     return sessionStorage.getItem('key');
                 }
