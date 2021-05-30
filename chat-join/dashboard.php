925d29e3c8ac369ee65489a66f150ef129c9d090
anthony lanzenberger <anthony.lanzenberger@gmail.com>
2021-05-30 17:22:13 +0200

    Update dashboard.php

--- chat-join/dashboard.php
+++ chat-join/dashboard.php
@@ -22,7 +22,6 @@
                 }, 3000);
             }
             function encryptKey() {
-                console.log("sakldjadslkdj: "+sessionStorage.length);
                 if (sessionStorage.length < 1) {
                     let encryptionKey = "";
                     let chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
